9bc36ee81b197bb7d2d3c6790c023759da54f4ae
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2022-01-24 23:48:21 +0330

    some ffuctions added.

--- doodle/searchmate/urls.py
+++ doodle/searchmate/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns= [
-path("",views.search)
+path("",views.search),
+path("add",views.add_document),
+path("delete",views.delete_document)
+
 ]
--- doodle/searchmate/views.py
+++ doodle/searchmate/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from searchmate.Document import WebDocument
+from django_elasticsearch_dsl.documents import Document
+from elasticsearch_dsl import document
+from searchmate.documents import WebDocument
 from typing import Text
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpRequest
+from searchmate.models import webDoc
 # Create your views here.
 
 
@@ -12,4 +15,23 @@ def search(request):
     
     for hit in s:
         print(hit.title)
+        print(hit.content)
+        print(hit.url)
     return render(request,"")
+
+
+
+
+def add_document(request):
+    current = webDoc(
+    title=request.GET['title'],
+    content=request.GET['content'],
+    url=request.GET['url'])
+    current.save() #seve to db
+    return HttpResponse(f"This Doc is saved successfully: {request.GET['title']} {request.GET['content']} {request.GET['url']}")
+
+
+def delete_document(request):
+    instance = webDoc.objects.get(title=request.GET['title'],content=request.GET['content'],url=request.GET['url'])
+    instance.delete()
+    return HttpResponse(f"This Doc is saved successfully: {request.GET['title']} {request.GET['content']} {request.GET['url']}")
