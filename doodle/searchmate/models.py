34ddb1ebede506c4d918c5b5944a124a2fa95c4a
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2022-01-27 23:23:32 +0330

    title maximum characters changed.

--- doodle/searchmate/models.py
+++ doodle/searchmate/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class webDoc(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
     content = models.TextField()
     url = models.URLField()
     
